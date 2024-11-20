06729bc7f606a2d2b5d896c19291a00649b0f470
Ahmed Esslaoui Bribri <ahmedesslaouibribri@Ahmeds-MacBook-Air.local>
2024-11-20 15:42:02 +0100

    Configure GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -5,8 +5,13 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/{repo-name}' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/{repo-name}' : '',
+  basePath: '/csv-analyzer',
+  assetPrefix: '/csv-analyzer/',
+  trailingSlash: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
+    return config;
+  },
 }
 
 module.exports = nextConfig
